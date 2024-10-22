ae167d4665c71c1ee5c37f07f7716fd7d7c46dec
sinkin' feeling <109888201+sinkinfeeling@users.noreply.github.com>
2024-10-22 23:34:41 +0530

    Create Calculator10.java

---
+++ src/Calculator10.java
@@ -0,0 +1,24 @@
+class Calculator10 {
+    // Overloaded method for addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class MethodOverloadingExample {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("Addition of two integers: " + calculator.add(5, 10));
+        System.out.println("Addition of two doubles: " + calculator.add(3.5, 2.7));
+        System.out.println("Addition of three integers: " + calculator.add(1, 2, 3));
+    }
+}
