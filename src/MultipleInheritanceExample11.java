90d0a2920f2b8b79d6da7e9c15456ef9b558d9eb
sinkin' feeling <109888201+sinkinfeeling@users.noreply.github.com>
2024-10-22 23:41:35 +0530

    Create MultipleInheritanceExample11.java

---
+++ src/MultipleInheritanceExample11.java
@@ -0,0 +1,23 @@
+interface Printable {
+    void print();
+}
+
+interface Showable {
+    void show();
+}
+
+class MultipleInheritanceExample11 implements Printable, Showable {
+    public void print() {
+        System.out.println("Printable interface method.");
+    }
+
+    public void show() {
+        System.out.println("Showable interface method.");
+    }
+
+    public static void main(String[] args) {
+        MultipleInheritanceExample obj = new MultipleInheritanceExample();
+        obj.print();
+        obj.show();
+    }
+}
