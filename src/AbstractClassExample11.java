076ddf501b49f1a45d969780ee73c6c37fba69d2
sinkin' feeling <109888201+sinkinfeeling@users.noreply.github.com>
2024-10-22 23:36:36 +0530

    Create AbstractClassExample11.java

---
+++ src/AbstractClassExample11.java
@@ -0,0 +1,21 @@
+abstract class Animal {
+    public abstract void sound();
+
+    public void eat() {
+        System.out.println("This animal is eating.");
+    }
+}
+
+class Dog extends Animal {
+    public void sound() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class AbstractClassExample11 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+        dog.eat();
+    }
+}
