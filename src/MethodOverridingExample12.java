90a9895eb0203f44b1763d2a4fbba73daab83ba5
sinkin' feeling <109888201+sinkinfeeling@users.noreply.github.com>
2024-10-22 23:42:13 +0530

    Create MethodOverridingExample12.java

---
+++ src/MethodOverridingExample12.java
@@ -0,0 +1,19 @@
+class Animal {
+    public void sound() {
+        System.out.println("Animal is making a sound.");
+    }
+}
+
+class Dog extends Animal {
+    public void sound() {
+        super.sound();  // Calling the parent class method
+        System.out.println("The dog barks.");
+    }
+}
+
+public class MethodOverridingExample12 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+    }
+}
